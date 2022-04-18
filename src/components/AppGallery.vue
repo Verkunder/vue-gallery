3e4526baa70dcd56b3fa6f89dd27366d29f79bdf
VerkunderWP <s.kapustin@webpractik.ru>
2022-04-18 14:08:14 +0300

    change gallery

--- src/components/AppGallery.vue
+++ src/components/AppGallery.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-    <h2>Галлерея картинок</h2>
+    <h2>Галерея картинок</h2>
     <AppLoader v-if="loading" />
     <div class="gallery">
       <div class="gallery__block" v-for="(item, id) in gallery" key="id">
@@ -24,6 +24,23 @@
         >
         </textarea>
       </div>
+      <div class="comment" v-if="bigImg.comments.length != 0">
+        <div
+          class="comment__data"
+          v-for="(item, id) in bigImg.comments"
+          key="id"
+        >
+          {{ item.date }}
+        </div>
+        <div
+          class="comment__title"
+          v-for="(item, id) in bigImg.comments"
+          key="id"
+        >
+          {{ item.text }}
+        </div>
+      </div>
+      <div class="comment__title" v-else>Комментариев нет</div>
     </template>
     <template v-slot:footer>
       <button class="modal-footer__button" @click="sendDataFunction()">
@@ -68,16 +85,22 @@ export default {
       this.$refs.modal.show = true;
     },
     sendDataFunction() {
-      console.log(this.id);
-      axios.post(
-        `https://boiling-refuge-66454.herokuapp.com/images/${this.id}/comments`,
-        {
-          name: this.name,
-          comment: this.comment,
-        }
-      );
-      this.comment = "";
-      this.name = "";
+      if (confirm("Оставить комментарий?")) {
+        axios.post(
+          `https://boiling-refuge-66454.herokuapp.com/images/${this.id}/comments`,
+          {
+            name: this.name,
+            comment: this.comment,
+          }
+        );
+        this.comment = "";
+        this.name = "";
+        alert("Комментарий отправлен");
+      } else {
+        alert("Комментарий не оставлен");
+        this.comment = "";
+        this.name = "";
+      }
     },
   },
   mounted() {
@@ -149,4 +172,38 @@ input[type="text"]
 textarea.modal-textarea
     margin: 10px
     padding: 10px
+
+.comment
+
+    &__title
+        margin-bottom: 6px
+        transition: color 0.3s ease-in-out
+        text-align: center
+        display: flex
+        padding: 0 30px
+        +alegreya-extrabold(24px, 150%)
+        +max-width($desktop)
+                text-align: left
+                margin-top: 8px
+                text-align: center
+                +alegreya-extrabold(20px, 150%)
+        +max-width($mobail)
+                text-align: start
+                +alegreya-extrabold(18px, 150%)
+
+    &__data
+            text-align: center
+            margin-bottom: 8px
+            display: block
+            +roboto(12px, 100%)
+            line-height: 18px
+            color: $iron
+            height: auto
+            display: flex
+            padding: 0 15px
+            +max-width($mobail)
+                    font-size: 11px
+                    line-height: 1.5
+                    margin-bottom: 4px
+                    text-align: left
 </style>
